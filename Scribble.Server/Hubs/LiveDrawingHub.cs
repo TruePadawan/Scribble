6036e2a2e858227cea47fd2bc03809acaa671ddf
Hermes <hermeschigoziri@gmail.com>
2026-02-03 18:08:00 +0100

    Add hub methods for broadcasting canvas events and collecting canvas state snapshot from room host

--- Scribble.Server/Hubs/LiveDrawingHub.cs
+++ Scribble.Server/Hubs/LiveDrawingHub.cs
@@ -1,16 +1,54 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
+using Scribble.Shared.Lib;
 
 namespace Scribble.Server.Hubs;
 
 public class LiveDrawingHub : Hub
 {
-    public async Task JoinRoom(string roomName)
+    private static readonly ConcurrentDictionary<string, List<string>> Rooms = new();
+
+    public async Task JoinRoom(string roomId)
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+
+        Rooms.AddOrUpdate(roomId,
+            [Context.ConnectionId],
+            // If room exists, add new client to room
+            (key, list) =>
+            {
+                list.Add(Context.ConnectionId);
+                return list;
+            });
+
+        var usersInRoom = Rooms[roomId];
+        if (usersInRoom.Count > 1)
+        {
+            var hostId = usersInRoom[0];
+            // Ask the room's host to send the canvas state to this new client
+            await Clients.Client(hostId).SendAsync("RequestCanvasState", Context.ConnectionId);
+        }
     }
 
-    public async Task LeaveRoom(string roomName)
+    public async Task LeaveRoom(string roomId)
     {
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+
+        var usersInRoom = Rooms[roomId];
+        usersInRoom.Remove(Context.ConnectionId);
+        if (usersInRoom.Count == 0)
+        {
+            Rooms.TryRemove(roomId, out _);
+        }
+    }
+
+    public async Task SendEvent(string roomId, Event canvasEvent)
+    {
+        await Clients.OthersInGroup(roomId).SendAsync("ReceiveEvent", canvasEvent);
+    }
+
+    public async Task SendCanvasStateToClient(string targetConnectionId, List<Stroke> currentStrokes)
+    {
+        await Clients.Client(targetConnectionId).SendAsync("ReceiveCanvasState", currentStrokes);
     }
 }
--- Scribble/Lib/LiveDrawingService.cs
+++ Scribble/Lib/LiveDrawingService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
+using Scribble.Shared.Lib;
 
 namespace Scribble.Lib;
 
@@ -9,12 +11,24 @@ public class LiveDrawingService(string serverUrl)
     private readonly HubConnection _connection =
         new HubConnectionBuilder().WithUrl(serverUrl).WithAutomaticReconnect().Build();
 
+    public HubConnectionState ConnectionState => _connection.State;
     public event Action? ConnectionStarted;
     public event Action? ConnectionStopped;
-    public HubConnectionState ConnectionState => _connection.State;
+    public event Action<Event>? EventReceived;
+    public event Action<string>? CanvasStateRequested;
+    public event Action<List<Stroke>>? CanvasStateReceived;
 
     public async Task StartAsync()
     {
+        // Listen for draw events from others in the room
+        _connection.On<Event>("ReceiveEvent", ev => EventReceived?.Invoke(ev));
+
+        // Only relevant when this client is the host, listens for requests for the canvas state from other clients
+        _connection.On<string>("RequestCanvasState", req => CanvasStateRequested?.Invoke(req));
+
+        // We're a new client joining a room, listens for the response from the host carrying the canvas state
+        _connection.On<List<Stroke>>("ReceiveCanvasState", req => CanvasStateReceived?.Invoke(req));
+
         await _connection.StartAsync();
         ConnectionStarted?.Invoke();
     }
@@ -35,6 +49,22 @@ public class LiveDrawingService(string serverUrl)
         else
         {
             throw new Exception($"Failed to join room, Hub connection state - {_connection.State}");
+        }
+    }
+
+    public async Task BroadcastEventAsync(string roomId, Event evt)
+    {
+        if (_connection.State == HubConnectionState.Connected)
+        {
+            await _connection.InvokeAsync("SendEvent", roomId, evt);
+        }
+    }
+
+    public async Task SendCanvasStateToClientAsync(string targetId, List<Stroke> strokes)
+    {
+        if (_connection.State == HubConnectionState.Connected)
+        {
+            await _connection.InvokeAsync("SendCanvasStateToClient", targetId, strokes);
         }
     }
 }
