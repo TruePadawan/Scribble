cb65d8934acf2bea358b986909279ce2103fefbc
Hermes <hermeschigoziri@gmail.com>
2026-02-04 10:05:47 +0100

    code clean up

--- Scribble.Shared/Lib/Event.cs
+++ Scribble.Shared/Lib/Event.cs
@@ -27,16 +27,12 @@ public abstract record Event
     public DateTime TimeStamp { get; init; } = DateTime.UtcNow;
 }
 
-public interface ITerminalEvent
-{
-}
-
 public abstract record StrokeEvent(Guid StrokeId) : Event;
 
 public record StartStrokeEvent(Guid StrokeId, SKPoint StartPoint, StrokePaint StrokePaint, StrokeTool ToolType)
     : StrokeEvent(StrokeId);
 
-public record EndStrokeEvent(Guid StrokeId) : StrokeEvent(StrokeId), ITerminalEvent;
+public record EndStrokeEvent(Guid StrokeId) : StrokeEvent(StrokeId);
 
 // PENCIL TOOL
 public record PencilStrokeLineToEvent(Guid StrokeId, SKPoint Point) : StrokeEvent(StrokeId);
@@ -46,21 +42,21 @@ public record StartEraseStrokeEvent(Guid StrokeId, SKPoint StartPoint) : StrokeE
 
 public record EraseStrokeLineToEvent(Guid StrokeId, SKPoint Point) : StrokeEvent(StrokeId);
 
-public record TriggerEraseEvent(Guid StrokeId) : StrokeEvent(StrokeId), ITerminalEvent;
+public record TriggerEraseEvent(Guid StrokeId) : StrokeEvent(StrokeId);
 
 // LINE + ARROW + RECTANGLE TOOL
 public record LineStrokeLineToEvent(Guid StrokeId, SKPoint EndPoint) : StrokeEvent(StrokeId);
 
 // TEXT TOOL
 public record AddTextEvent(Guid StrokeId, SKPoint Position, string Text, StrokePaint Paint)
-    : StrokeEvent(StrokeId), ITerminalEvent;
+    : StrokeEvent(StrokeId);
 
 // SELECT TOOL
 public record CreateSelectionBoundEvent(Guid BoundId, SKPoint StartPoint) : StrokeEvent(BoundId);
 
 public record IncreaseSelectionBoundEvent(Guid BoundId, SKPoint Point) : StrokeEvent(BoundId);
 
-public record EndSelectionEvent(Guid BoundId) : StrokeEvent(BoundId), ITerminalEvent;
+public record EndSelectionEvent(Guid BoundId) : StrokeEvent(BoundId);
 
 public record MoveStrokesEvent(Guid BoundId, SKPoint Delta) : StrokeEvent(BoundId);
 
@@ -69,7 +65,7 @@ public record RotateStrokesEvent(Guid BoundId, float DegreesRad, SKPoint Center)
 public record ScaleStrokesEvent(Guid BoundId, SKPoint Scale, SKPoint Center) : StrokeEvent(BoundId);
 
 // MISC
-public record RestoreCanvasEvent(List<Stroke> Strokes) : Event, ITerminalEvent;
+public record RestoreCanvasEvent(List<Stroke> Strokes) : Event;
 
 public record UndoEvent(Guid TargetStrokeId) : StrokeEvent(TargetStrokeId);
 
