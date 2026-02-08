9f9e3f5b2d88d8ecf88ccbc509d78850364985ad
Hermes <hermeschigoziri@gmail.com>
2026-02-08 15:41:29 +0100

    chore: Remove OpenApi dependency from Scribble Server

--- Scribble.Server/Program.cs
+++ Scribble.Server/Program.cs
@@ -2,9 +2,6 @@ using Scribble.Server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddSignalR(options =>
 {
     options.EnableDetailedErrors = true;
@@ -12,12 +9,6 @@ builder.Services.AddSignalR(options =>
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
