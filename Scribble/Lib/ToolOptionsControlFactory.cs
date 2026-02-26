90e66d6225e3813c9794afc5ab290b6a9959f571
Hermes <hermeschigoziri@gmail.com>
2026-02-26 10:10:01 +0100

    add ToolOptionsValues class to encapsulate tool option properties

--- Scribble/Lib/ToolOptionsControlFactory.cs
+++ Scribble/Lib/ToolOptionsControlFactory.cs
@@ -11,17 +11,6 @@ using Scribble.Shared.Lib;
 
 namespace Scribble.Lib;
 
-public record ToolOptionsValues
-{
-    public float StrokeThickness { get; set; } = 1f;
-    public Color StrokeColor { get; set; } = Colors.White;
-    public StrokeStyle StrokeStyle { get; set; } = StrokeStyle.Solid;
-    public float[]? DashIntervals { get; set; }
-    public Color FillColor { get; set; } = Colors.Transparent;
-    public EdgeType EdgeType { get; set; } = EdgeType.Sharp;
-    public float FontSize { get; set; } = 10;
-}
-
 internal static class ToolOptionsControlFactory
 {
     public static ColorPicker GetStrokeColorOption()
