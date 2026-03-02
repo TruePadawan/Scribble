851a107d98c7e94de5d1274ccf90ecb7c093ba7c
Hermes <hermeschigoziri@gmail.com>
2026-03-02 18:35:37 +0100

    chore: document the Utilities static class

--- Scribble/Utils/Utilities.cs
+++ Scribble/Utils/Utilities.cs
@@ -7,23 +7,44 @@ using SkiaSharp;
 
 namespace Scribble.Utils;
 
+/// <summary>
+/// Contains various utility methods
+/// </summary>
 public static class Utilities
 {
+    /// <summary>
+    /// Converts a Color to a SkiaSharp Color
+    /// </summary>
+    /// <returns>The equivalent <see cref="SKColor">SKColor</see> object</returns>
     public static SKColor ToSkColor(Color color)
     {
         return new SKColor(color.R, color.G, color.B, color.A);
     }
 
+    /// <summary>
+    /// Converts a SkiaSharp Color to a Color
+    /// </summary>
+    /// <returns>The equivalent <see cref="Color">Color</see> object</returns>
     public static Color FromSkColor(SKColor color)
     {
         return new Color(color.Alpha, color.Red, color.Green, color.Blue);
     }
 
+    /// <summary>
+    /// Returns the size of the rectangle formed by the two points
+    /// </summary>
+    /// <param name="start">Starting point</param>
+    /// <param name="end">End point</param>
+    /// <returns>A <see cref="SKSize">SKSize</see> object representing the size of the rectangle</returns>
     public static SKSize GetSize(SKPoint start, SKPoint end)
     {
         return new SKSize(Math.Abs(start.X - end.X), Math.Abs(start.Y - end.Y));
     }
 
+    /// <summary>
+    /// Converts a Point to a SkiaSharp Point
+    /// </summary>
+    /// <returns>The equivalent <see cref="Point">Point</see> object</returns>
     public static SKPoint ToSkPoint(Point coord)
     {
         return new SKPoint((float)coord.X, (float)coord.Y);
