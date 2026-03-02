16f510280f5f8c1a3fa5423b02e7c165c804a49c
Hermes <hermeschigoziri@gmail.com>
2026-03-02 18:24:56 +0100

    chore: documentation

--- Scribble/Lib/CollaborativeDrawing/CollaborativeDrawingRoom.cs
+++ Scribble/Lib/CollaborativeDrawing/CollaborativeDrawingRoom.cs
@@ -3,6 +3,12 @@ using Scribble.Shared.Lib;
 
 namespace Scribble.Lib.CollaborativeDrawing;
 
+/// <summary>
+/// Represents a multi-user drawing room
+/// </summary>
+/// <param name="roomId">The unique ID of the room</param>
+/// <param name="connectionId">The client's connection ID from SignalR</param>
+/// <param name="displayName">The client's display name</param>
 public class CollaborativeDrawingRoom(string roomId, string connectionId, string displayName)
 {
     public string RoomId { get; } = roomId;
