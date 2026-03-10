9e025dd242ba1fede1af5d3919a7a98e0ab74384
Hermes <hermeschigoziri@gmail.com>
2026-03-10 08:51:59 +0100

    refactor: move SKPathJsonConverter to separate file

--- Scribble.Shared/Lib/CanvasElements.cs
+++ Scribble.Shared/Lib/CanvasElements.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Scribble.Shared.Converters;
 using SkiaSharp;
 
 namespace Scribble.Shared.Lib;
@@ -114,29 +115,4 @@ public enum EdgeType
 {
     Sharp,
     Rounded
-}
-
-/// <summary>
-/// Converts an SKPath to and from an SVG path string
-/// This preserves Moves, Lines, Curves, and Shapes automatically
-/// </summary>
-public class SKPathJsonConverter : JsonConverter<SKPath>
-{
-    public override SKPath Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        string? svgPathData = reader.GetString();
-
-        if (string.IsNullOrEmpty(svgPathData))
-        {
-            return new SKPath();
-        }
-
-        return SKPath.ParseSvgPathData(svgPathData);
-    }
-
-    public override void Write(Utf8JsonWriter writer, SKPath value, JsonSerializerOptions options)
-    {
-        string svgPathData = value.ToSvgPathData();
-        writer.WriteStringValue(svgPathData);
-    }
 }
